c712546e5d0193ac2b19510282459634fcacea2d
WylieYYYY <wylieyyyy@gmail.com>
2024-01-29 23:14:34 +0000

    Added persistent Lutris game configurations

--- modules/applications/lutris.nix
+++ modules/applications/lutris.nix
@@ -41,6 +41,40 @@ let
     };
   };
 
+  # Installer specific options.
+  installerModule = lib.types.submodule {
+    options = {
+      slug = lib.mkOption {
+        type = lib.types.str;
+        description = "Slug for the installer.";
+      };
+      sha256 = lib.mkOption {
+        type = lib.types.str;
+        default = null;
+        description = "Hash value for the installer script.";
+      };
+    };
+  };
+
+  # Game specific options.
+  gameModule = lib.types.submodule {
+    options = {
+      bannerSha256 = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+        description = "Hash value for the banner art.";
+      };
+      installer = lib.mkOption {
+        type = installerModule;
+        apply = installer: builtins.elemAt (builtins.fromJSON (builtins.readFile (pkgs.fetchurl {
+          url = "https://lutris.net/api/installers/${installer.slug}?format=json";
+          inherit (installer) sha256;
+        }))).results 0;
+        description = "Slug for the installer.";
+      };
+    };
+  };
+
   # Fetches the online listing and match the version and architecture.
   # todo: Add cache option for the listing in case Lutris remove runners.
   downloadDetail = runnerName: version: (lib.findFirst (variant:
@@ -61,6 +95,9 @@ let
 in
 
 {
+
+  imports = [ ./../system/writableHomeFile.nix ];
+
   options.programs.lutris = {
     enable = lib.mkEnableOption "lutris, an open gaming platform";
 
@@ -74,6 +111,19 @@ in
       type = globalOptionsModule;
       default = { prime = false; };
       description = "Global system options.";
+    };
+
+    gameDirectory = lib.mkOption {
+      type = lib.types.str;
+      description = "Parent directory for games.";
+    };
+
+    games = lib.mkOption {
+      type = lib.types.submodule {
+        freeformType = lib.types.attrsOf gameModule;
+      };
+      default = { };
+      description = "Registered games in the database.";
     };
 
     runners = lib.mkOption {
@@ -94,12 +144,84 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = [ cfg.package ];
 
-    xdg.configFile."lutris/system.yml".text = lib.generators.toYAML { } {
-      system = cfg.globalOptions;
-    };
+    home.file = lib.mkMerge (builtins.map (game: lib.mkIf (game.bannerSha256 != null) {
+      "${config.xdg.cacheHome}/lutris/banners/${game.installer.game_slug}.jpg"
+          .source = pkgs.fetchurl {
+        url = "https://lutris.net/games/banner/${game.installer.game_slug}.jpg";
+        sha256 = game.bannerSha256;
+      };
+    }) (builtins.attrValues cfg.games));
+
+    xdg.configFile = let
+      installers = builtins.map (game: {
+        "lutris/games/${game.installer.slug}.yml".text = builtins.replaceStrings
+            [
+              "$GAMEDIR"
+              "STAGING_SHARED_MEMORY: 1"
+              "WINE_LARGE_ADDRESS_AWARE: 1"
+              "__GL_SHADER_DISK_CACHE: 1"
+            ]
+            [
+              "${cfg.gameDirectory}/${game.installer.game_slug}"
+              "STAGING_SHARED_MEMORY: '1'"
+              "WINE_LARGE_ADDRESS_AWARE: '1'"
+              "__GL_SHADER_DISK_CACHE: '1'"
+            ]
+            game.installer.content;
+      }) (builtins.attrValues cfg.games);
+    in lib.mkMerge ([{
+      "lutris/system.yml".text = lib.generators.toYAML { } {
+        system = cfg.globalOptions;
+      };
+    }] ++ installers);
 
     xdg.dataFile = lib.mkMerge (lib.foldlAttrs (acc: name: value:
       acc ++ generateRunnerFiles name value
     ) [ ] cfg.runners);
+
+    home.writableFile."${config.xdg.dataHome}/lutris/pga.db".source = let
+      escape = builtins.replaceStrings [ "'" ] [ "''" ];
+      insertStatements = (lib.foldlAttrs(acc: name: value: acc + ''
+        INSERT INTO games (
+          name, slug, installer_slug, platform, runner,
+          directory, installed, installed_at, configpath
+        ) VALUES (
+          '${escape name}', '${escape value.installer.game_slug}',
+          '${escape value.installer.slug}',
+          'NixOS', '${escape value.installer.runner}',
+          '${escape cfg.gameDirectory}/${escape value.installer.game_slug}',
+          1, 0, '${escape value.installer.slug}'
+        );
+      '') "" cfg.games);
+    in pkgs.runCommand "lutris-db" { } ''
+      ${lib.getExe pkgs.sqlite} $out <<EOF
+        CREATE TABLE games (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          sortname TEXT,
+          slug TEXT,
+          installer_slug TEXT,
+          parent_slug TEXT,
+          platform TEXT,
+          runner TEXT,
+          executable TEXT,
+          directory TEXT,
+          updated DATETIME,
+          lastplayed INTEGER,
+          installed INTEGER,
+          installed_at INTEGER,
+          year INTEGER,
+          configpath TEXT,
+          has_custom_banner INTEGER,
+          has_custom_icon INTEGER,
+          has_custom_coverart_big INTEGER,
+          playtime REAL, hidden INTEGER,
+          service TEXT,
+          service_id TEXT,
+          discord_id TEXT
+        );
+        ${insertStatements}
+      EOF
+    '';
   };
 }
